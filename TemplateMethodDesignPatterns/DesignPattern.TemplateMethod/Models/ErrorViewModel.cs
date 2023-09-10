7a6e86d01e5ae40ab45ff05b7e68fa70123bccfb
zehrasbr <zehraisbr@gmail.com>
2023-09-10 23:59:31 +0300

    Add project files.

---
+++ TemplateMethodDesignPatterns/DesignPattern.TemplateMethod/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DesignPattern.TemplateMethod.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TemplateMethodDesignPatterns/DesignPattern.TemplateMethod/TemplatePattern/NetflixPlans.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPattern.TemplateMethod.TemplatePattern
+{
+    public abstract class NetflixPlans
+    {
+        public abstract string PlanType  { get; set; }
+        public abstract int CountPerson  { get; set; }
+        public abstract decimal Price  { get; set; }
+        public abstract string Resolution  { get; set; }
+        public abstract string Content  { get; set; }
+    }
+}

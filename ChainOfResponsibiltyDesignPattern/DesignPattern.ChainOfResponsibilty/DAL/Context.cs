09fd449f03d3c46034695c1c1e2090c5cd317459
zehrasbr <zehraisbr@gmail.com>
2023-11-12 20:38:23 +0300

    context

---
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibilty.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=UNKNOWN\\SQLEXPRESS;initial catalog=DesignPattern1;integrated security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses {  get; set; }
+    }
+}

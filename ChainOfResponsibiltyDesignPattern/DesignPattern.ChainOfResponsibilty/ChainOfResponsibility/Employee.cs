f7020114f886adf8ffed465b162fbafd3aa3192f
zehrasbr <zehraisbr@gmail.com>
2023-11-13 14:10:12 +0300

    employee

--- ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/Employee.cs
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/Employee.cs
@@ -1,6 +1,14 @@
-﻿namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibility
+﻿using DesignPattern.ChainOfResponsibilty.Models;
+
+namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibility
 {
     public abstract class Employee
     {
+        protected Employee NextApprover;
+        public void SetNextApprover(Employee superVisor)
+        {
+            this.NextApprover = superVisor;
+        }
+        public abstract void ProcessRequest(CustomerProcessViewModel req);
     }
 }

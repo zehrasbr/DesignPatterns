951a6a0267d3be32af0d3762f7a0e9b8161235f6
zehrasbr <zehraisbr@gmail.com>
2023-11-13 14:52:49 +0300

    managerassistant

--- ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/ManagerAssistant.cs
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/ManagerAssistant.cs
@@ -3,28 +3,28 @@ using DesignPattern.ChainOfResponsibilty.Models;
 
 namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibility
 {
-    public class Treasurer : Employee
+    public class ManagerAssistant : Employee
     {
         public override void ProcessRequest(CustomerProcessViewModel req)
         {
             Context context = new Context();
-            if(req.Amount<=100000)
+            if(req.Amount<=15000)
             {
                 CustomerProcess customerProcess = new CustomerProcess();
                 customerProcess.Amount = req.Amount.ToString();
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName = "Veznedar - Zehra İsbir";
+                customerProcess.EmployeeName = "Şube Müdür Yardımcısı - Melike";
                 customerProcess.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
                 context.CustomerProcesses.Add(customerProcess);
                 context.SaveChanges();
             }
-            else if(NextApprover!=null)
+            else if (NextApprover != null)
             {
                 CustomerProcess customerProcess = new CustomerProcess();
                 customerProcess.Amount = req.Amount.ToString();
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName= "Veznedar - Zehra İsbir";
-                customerProcess.Description= "Para Çekme Tutarı Veznedarın Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Şube Müdür Yardımcısına Yönlendirildi";
+                customerProcess.EmployeeName = "Şube ";
+                customerProcess.Description = "Para Çekme Tutarı Şube Müdür Yardımcısının Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Şube Müdürüne Yönlendirildi";
                 context.CustomerProcesses.Add(customerProcess);
                 context.SaveChanges();
                 NextApprover.ProcessRequest(req);

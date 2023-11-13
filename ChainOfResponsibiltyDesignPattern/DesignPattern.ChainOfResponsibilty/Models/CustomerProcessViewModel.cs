9079c972b0e6be4cba58adcfe0baf88f2df9527d
zehrasbr <zehraisbr@gmail.com>
2023-11-13 14:20:52 +0300

    .

--- ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/Models/CustomerProcessViewModel.cs
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/Models/CustomerProcessViewModel.cs
@@ -4,7 +4,7 @@
     {
         public int CustomerProcessID { get; set; }
         public string Name { get; set; }
-        public string Amount { get; set; }
+        public int Amount { get; set; }
         public string EmployeeName { get; set; }
         public string Description { get; set; }
     }

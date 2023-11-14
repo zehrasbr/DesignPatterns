f4c4e0d6318b7cb08793ddb9f9b7533f2b5b5c3e
zehrasbr <zehraisbr@gmail.com>
2023-11-14 20:16:10 +0300

    update

---
+++ CQRSDesignPattern/DesignPattern.CQRS/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using DesignPattern.CQRS.CQRSPattern.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.CQRS.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+
+        public DefaultController(GetProductQueryHandler getProductQueryHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+    }
+}
--- CQRSDesignPattern/DesignPattern.CQRS/Startup.cs
+++ CQRSDesignPattern/DesignPattern.CQRS/Startup.cs
@@ -1,3 +1,5 @@
+using DesignPattern.CQRS.CQRSPattern.Handlers;
+using DesignPattern.CQRS.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +25,8 @@ namespace DesignPattern.CQRS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddScoped<GetProductQueryHandler>();
             services.AddControllersWithViews();
         }
 

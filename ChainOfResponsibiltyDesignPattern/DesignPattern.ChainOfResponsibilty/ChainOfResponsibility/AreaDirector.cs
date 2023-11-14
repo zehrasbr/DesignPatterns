1858ac77eec4e31c57c0d46e809748d5d19689ab
zehrasbr <zehraisbr@gmail.com>
2023-11-14 18:50:56 +0300

    areadirector

---
+++ ChainOfResponsibiltyDesignPattern/DesignPattern.ChainOfResponsibilty/ChainOfResponsibility/AreaDirector.cs
@@ -0,0 +1,34 @@
+﻿using DesignPattern.ChainOfResponsibilty.DAL;
+using DesignPattern.ChainOfResponsibilty.Models;
+
+namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if (req.Amount <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep";
+                customerProcess.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            //if olmayacak çünkü artık bundan sonra yönlendirecek bir mevki yok.
+            else
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep ";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Direktötürünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi, Müşterinin Günlük Maksimum Çekebileceği Tutar 400.000 ₺ olup daha fazlası için birden fazla gün şubeye gelmesi gerekli...";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+        }
+    }
+}

830d3c246088976d4fa0e0d26a67cf6738916abf
zehrasbr <zehraisbr@gmail.com>
2023-11-14 19:45:51 +0300

    cqrs

---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,6 @@
+﻿namespace DesignPattern.CQRS.DAL
+{
+    public class Context
+    {
+    }
+}
---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern.CQRS.DAL
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}

3f367e1457e1059731f8fa40623524d253cb2b03
zehrasbr <zehraisbr@gmail.com>
2023-11-14 20:00:50 +0300

    handlernd result

---
+++ CQRSDesignPattern/DesignPattern.CQRS/CQRSPattern/Handlers/GetProductQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.CQRS.CQRSPattern.Results;
+using DesignPattern.CQRS.DAL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPattern.CQRS.CQRSPattern.Handlers
+{
+    public class GetProductQueryHandler
+    {
+        private readonly Context _context;
+        public GetProductQueryHandler(Context context)
+        {
+            _context = context;
+        }
+        public List<GetProductQueryResult> Handle()
+        {
+            var values = _context.Products.Select(x => new GetProductQueryResult
+            {
+                ID = x.ProductID,
+                Price = x.Price,
+                ProductName = x.Name,
+                Stock = x.Stock,
+            }).ToList();
+            return values;
+        }
+    }
+}
---
+++ CQRSDesignPattern/DesignPattern.CQRS/CQRSPattern/Results/GetProductQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.CQRS.CQRSPattern.Results
+{
+    public class GetProductQueryResult
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}

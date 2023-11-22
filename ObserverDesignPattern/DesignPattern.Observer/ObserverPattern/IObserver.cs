3f47632f4441a51d9650c242f815aa1fa06c0a83
zehrasbr <zehraisbr@gmail.com>
2023-11-22 18:34:09 +0300

    .

---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/IObserver.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Observer.DAL;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public interface IObserver
+    {
+        void CreateNewUser(AppUser appUser);
+    }
+}
---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/ObserverObject.cs
@@ -0,0 +1,29 @@
+﻿using DesignPattern.Observer.DAL;
+using System.Collections.Generic;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class ObserverObject
+    {
+        private readonly List<IObserver> _observers;
+        public ObserverObject()
+        {
+            _observers = new List<IObserver>();
+        }
+        public void RegisterObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+        public void RemoverObserver(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+        public void NotifyObserver(AppUser appUser)
+        {
+            _observers.ForEach(x =>
+            {
+                x.CreateNewUser(appUser);
+            });
+        }
+    }
+}

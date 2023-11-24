d3a07c6eb0cbe023f2dd3350c65a70488d8657ae
zehrasbr <zehraisbr@gmail.com>
2023-11-24 12:04:15 +0300

    class

---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateDiscountCode.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Observer.DAL;
+using System;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class CreateDiscountCode : IObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+        Context context = new Context();
+
+        public CreateDiscountCode(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateNewUser(AppUser appUser)
+        {
+            context.Discounts.Add(new Discount
+            {
+                DiscountCode = "DERGIMART",
+                DiscountAmount = 35,
+                DiscountCodeStatus = true
+            });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateMagazineAnnocuncement.cs
@@ -0,0 +1,26 @@
+﻿using DesignPattern.Observer.DAL;
+using System;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class CreateMagazineAnnocuncement : IObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+        Context context = new Context();
+        public CreateMagazineAnnocuncement(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void CreateNewUser(AppUser appUser)
+        {
+            context.UserProcesses.Add(new UserProcess
+            {
+                NameSurname = appUser.Name + " " + appUser.Surname,
+                Magazine = "Bilim Dergisi",
+                Content = "Bilim dergimizin Mart sayısı 1 Martta evinize ulaştırılacaktır, konular gezegenlerdir"
+
+            });
+            context.SaveChanges();
+        }
+    }
+}
